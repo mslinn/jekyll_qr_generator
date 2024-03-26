c574ce702a3105d16fdee7a00b7094e05291854f
Mike Slinn <mslinn@mslinn.com>
2024-03-26 15:59:37 -0400

    -

--- lib/page_data.rb
+++ lib/page_data.rb
@@ -5,11 +5,12 @@ module Jekyll
   class QRPath
     def self.hook_proc
       proc { |page|
+        site = page.site
         qr_dir = File.dirname "/assets/images/qrcodes#{Pathname.new(page.url).to_path}"
         basename = page.respond_to?(:basename_without_ext) ? page.basename_without_ext : page.basename
-        filepath = "#{qr_dir}/#{basename}.svg"
-        puts "QRPath: #{filepath}"
-        page.data['qrcode'] = filepath
+        qr_path = "https://#{site.config['domain']}#{qr_dir}/#{basename}.svg"
+        puts "QRPath: #{qr_path}"
+        page.data['qrcode'] = qr_path
       }
     end
 
--- lib/qr_generator.rb
+++ lib/qr_generator.rb
@@ -10,6 +10,15 @@ class Generator < Jekyll::Generator
     @site = site
     @qr_path = 'assets/images/qrcodes'
     @url_base = @site.config['domain']
+
+    unless @domain
+      puts <<~END_ERROR
+        Error: _config.yml must have an entry for domain. For example:
+          domain: www.my_domain.com
+      END_ERROR
+      exit 2
+    end
+
     FileUtils.mkdir_p @qr_path
     items = (@site.all_collections + @site.pages)
     items.each { |x| write_qrcode x }
@@ -18,17 +27,14 @@ class Generator < Jekyll::Generator
   def write_qrcode(apage)
     ext = apage.extname
     filepath_no_ext = URI(apage.url).path.delete_suffix ext
-    return if filepath_no_ext.end_with? '/'
-
+    filepath_no_ext += 'index' if filepath_no_ext.end_with? '/'
     filepath = "#{filepath_no_ext}.svg"
     static_file = Jekyll::StaticFile.new(@site, @site.source, @qr_path, filepath)
     return if @site.static_files.include? static_file
 
-    filename_fq = "#{@site.source}/#{@qr_path}#{filepath}"
-
-    url = "#{@url_base}#{apage.url}"
-    puts "Writing QR code to #{filename_fq}"
-    qrcode = RQRCode::QRCode.new(url)
+    url = "https://#{@url_base}#{apage.url}"
+    puts "Creating QR Code for #{url}"
+    qrcode = RQRCode::QRCode.new url
 
     rendered_svg = qrcode.as_svg(
       color:    :yellow,
@@ -36,10 +42,14 @@ class Generator < Jekyll::Generator
       use_path: true,
       viewbox:  true
     )
+
+    filename_fq = "#{@site.source}/#{@qr_path}#{filepath}"
+    puts "Writing QR code to #{filename_fq}"
     FileUtils.mkdir_p File.dirname filename_fq
     _bytes_written = File.write(filename_fq, rendered_svg.to_s)
     # puts "#{_bytes_written} bytes written to #{filename_fq}"
 
+    puts "Marking #{static_file} for preservation"
     @site.static_files << static_file
   end
 end
